adeb499b794dc17c4b493aca7f9d6aa6f513b660
Caili Li <cailiudel@gmail.com>
2020-12-28 23:20:50 +0000

    save

---
+++ src/communication/src/bidi_tcp_socket.cpp
@@ -0,0 +1,13 @@
+#include <thread>
+
+#include "bidi_tcp_socket.h"
+
+namespace relay {
+namespace communication {
+
+BiDirectionalTCPSocket::BiDirectionalTCPSocket(
+    TCPSocket* sock) {
+}
+
+}
+}
