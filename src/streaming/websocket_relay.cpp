df090a05e6473d955ed0a927741e2874024aff38
Caili Li <caili@udel.edu>
2021-03-05 10:13:19 -0800

    ws relay untested

--- src/streaming/websocket_relay.cpp
+++ src/streaming/websocket_relay.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <iostream>
 #include <memory>
+#include <set>
 #include <string>
 #include <thread>
 
@@ -22,9 +23,34 @@
 
 
 DEFINE_string(foreign_addr, "127.0.0.1", "Foreign address");
-DEFINE_int32(foreign_port, 6000, "Foreign port");
+DEFINE_int32(foreign_port, 6001, "Foreign port");
+DEFINE_int32(local_port, 8080, "Local port");
+
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
 
 typedef websocketpp::server<websocketpp::config::asio> Server;
+
+enum JsmpegTrameType {
+    jsmpeg_frame_type_video = 0xFA010000,
+    jsmpeg_frame_type_audio = 0xFB010000
+};
+
+struct JsmpegFrame {
+    JsmpegTrameType type;
+    int size;
+    char data[0];
+};
+
+struct JsmpegHeader {
+    unsigned char magic[4];
+    unsigned short width;
+    unsigned short height;
+};
+
+std::set<websocketpp::connection_hdl, 
+         std::owner_less<websocketpp::connection_hdl>> connection_list;
 
 std::atomic_bool running;
 
@@ -32,8 +58,26 @@ void myHandler(int s){
   running.store(false);
 }
 
-void onOpen(websocketpp::connection_hdl hdl) {
-{
+inline int swap_int16(int in) {
+  return ((in>>8)&0xff) | ((in<<8)&0xff00);
+}
+
+void onOpen(Server* s, websocketpp::connection_hdl hdl) {
+  JsmpegHeader header = 
+      {{'j','s','m','p'}, swap_int16(640),  swap_int16(360)};
+
+  try {
+    s->send(hdl, &header, sizeof(header), websocketpp::frame::opcode::binary);
+  } catch (websocketpp::exception const & e) {
+    LOG(ERROR) << "Error sending message: " << e.what();
+  }
+  connection_list.insert(hdl);
+  LOG(INFO) << "Client connected";
+}
+
+void onClose(Server* s, websocketpp::connection_hdl hdl) {
+  connection_list.erase(hdl);
+  LOG(INFO) << "Client disconnected";
 }
 
 int main(int argc, char** argv) {
@@ -53,9 +97,60 @@ int main(int argc, char** argv) {
   relay_server.clear_access_channels(websocketpp::log::alevel::frame_payload);
   relay_server.init_asio();
 
-  m_server.set_open_handler(bind(&broadcast_server::on_open,this,::_1));
+  relay_server.set_open_handler(bind(&onOpen, &relay_server, ::_1));
+  relay_server.set_close_handler(bind(&onClose, &relay_server, ::_1));
 
   running.store(true);
+  try {
+    LOG(INFO) << "Connecting to server: "
+              << FLAGS_foreign_addr 
+              << ":" 
+              << FLAGS_foreign_port;
+
+    relay::communication::BiDirectionalTCPSocket bidi_sock(
+        new TCPSocket(FLAGS_foreign_addr, FLAGS_foreign_port));
+
+    relay::codec::H264Decoder decoder;
+
+    relay_server.listen(FLAGS_local_port);
+    relay_server.start_accept();
+    relay_server.run();
+
+    while (bidi_sock.running()) {
+      if (running.load()) {
+        bidi_sock.comsume([&](std::deque<std::string>& buffer){
+          while (!buffer.empty()) {
+            decoder.push(std::move(buffer.front()));
+            buffer.pop_front();
+          }
+        });
+
+        bool got_decoded_frame = false;
+        decoder.comsume([&](std::deque<std::string>& buffer) {
+          if (!buffer.empty()) {
+            for (auto it : connection_list) {
+              try {
+                relay_server.send(
+                   it, buffer.front(), websocketpp::frame::opcode::binary);
+              } catch (websocketpp::exception const & e) {
+                LOG(ERROR) << "Error broadcasting message: " << e.what();
+              }
+            }
+            buffer.pop_front();
+            got_decoded_frame = true;
+          }
+        });
+      } else {
+        bidi_sock.stop();
+      }
+    }
+    bidi_sock.join();
+    decoder.stop();
+    decoder.join();
+  } catch (SocketException &e) {
+    LOG(FATAL) << "Error occurred when accepting connection: " << e.what();
+  }
+  LOG(INFO) << "Bye bye";
 
   return 0;
 }
